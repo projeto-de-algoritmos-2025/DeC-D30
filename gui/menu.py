b17b0a44592486676721bb917a64a5ceb9c8ac57
eduardodpms <eduardopms19@hotmail.com>
2025-11-07 19:53:47 -0300

    feat: implementar edição de pontos
    Co-authored-by: pedroeverton217 <pedro.everton217@gmail.com>

--- gui/menu.py
+++ gui/menu.py
@@ -9,7 +9,7 @@ class App():
     def __init__(self, root, db_path, w=1000, h=600, g=50, max=1000):
         self.root = root
         self.db_path = db_path
-        self.w, self.h, self.g = w, h, g
+        self.w, self.h, self.g = int(w/2), int(h/2), g
         self.max = max
         self.points, self.ids = {}, {}
 
@@ -19,81 +19,94 @@ class App():
         frame.pack()
         
         # Plota o canvas e linhas/textos de referência
-        self.canvas = tk.Canvas(frame, background='white', width=w, height=h)
+        self.canvas = tk.Canvas(frame, background='white', width=2*self.w, height=2*self.h)
         self.canvas.grid(row=1, column=0, columnspan=3, rowspan=3)
-        self.canvas.create_line(0, h/2, w, h/2, arrow=tk.LAST, width=2) # Eixo X
-        self.canvas.create_line(w/2, 0, w/2, h, arrow=tk.FIRST, width=2) # Eixo Y
-        self.canvas.create_text(w/2-11, h/2+7, text='0,0', font=("Arial", 10)) # Marcação da origem
-        self.canvas.create_text(w/2+10, 10, text=f'X', font=("Arial", 10, "bold")) # Marcação do eixo X
-        self.canvas.create_text(w-8, h/2-10, text=f'Y', font=("Arial", 10, "bold")) # Marcação do eixo Y
-        self.canvas.create_text(w/2-11, h/2+1-g, text=f'+{g}', font=("Arial", 8)) # Referência do grid (X)
-        self.canvas.create_text(w/2-3+g, h/2+7, text=f'+{g}', font=("Arial", 8)) # Referência do grid (Y)
-        self.canvas.create_text(w-12, h/2+10, text=f'+{int(w/2)}', font=("Arial", 8)) # X máximo
-        self.canvas.create_text(14, h/2+10, text=f'-{int(w/2)}', font=("Arial", 8)) # X mínimo
-        self.canvas.create_text(w/2-16, 8, text=f'+{int(h/2)}', font=("Arial", 8)) # Y máximo
-        self.canvas.create_text(w/2-16, h-5, text=f'-{int(h/2)}', font=("Arial", 8)) # Y mínimo
+        self.canvas.create_line(0, self.h, 2*self.w, self.h, arrow=tk.LAST, width=2) # Eixo X
+        self.canvas.create_line(self.w, 0, self.w, 2*self.h, arrow=tk.FIRST, width=2) # Eixo Y
+        self.canvas.create_text(self.w-11, self.h+7, text='0,0', font=("Arial", 10)) # Marcação da origem
+        self.canvas.create_text(self.w+10, 10, text=f'X', font=("Arial", 10, "bold")) # Marcação do eixo X
+        self.canvas.create_text(2*self.w-8, self.h-10, text=f'Y', font=("Arial", 10, "bold")) # Marcação do eixo Y
+        self.canvas.create_text(self.w-11, self.h+1-self.g, text=f'+{g}', font=("Arial", 8)) # Referência do grid (X)
+        self.canvas.create_text(self.w-3+self.g, self.h+7, text=f'+{g}', font=("Arial", 8)) # Referência do grid (Y)
+        self.canvas.create_text(2*self.w-12, self.h+10, text=f'+{int(w)}', font=("Arial", 8)) # X máximo
+        self.canvas.create_text(14, self.h+10, text=f'-{int(w)}', font=("Arial", 8)) # X mínimo
+        self.canvas.create_text(self.w-16, 8, text=f'+{int(h)}', font=("Arial", 8)) # Y máximo
+        self.canvas.create_text(self.w-16, 2*self.h-5, text=f'-{int(h)}', font=("Arial", 8)) # Y mínimo
 
-        for x in range(0, w, g):
-            self.canvas.create_line(x, 0, x, h, fill="gray", dash=(2, 2)) # Plota grids verticais
-        for y in range(0, h, g):
-            self.canvas.create_line(0, y, w, y, fill="gray", dash=(2, 2)) # Plota grids horizontais
+        for x in range(0, 2*self.w, self.g):
+            self.canvas.create_line(x, 0, x, 2*self.h, fill="gray", dash=(2, 2)) # Plota grids verticais
+        for y in range(0, 2*self.h, self.g):
+            self.canvas.create_line(0, y, 2*self.w, y, fill="gray", dash=(2, 2)) # Plota grids horizontais
 
         # Ações de click no canvas
-        self.canvas.bind("<Button-1>", lambda event: self.on_add_edit(self.add_point, event))
-        self.canvas.bind("<Button-2>", lambda event: self.on_add_edit(self.edit_point, event))
+        self.canvas.bind("<Button-1>", self.on_add)
+        self.canvas.bind("<Button-2>", self.on_edit)
         self.canvas.bind("<Button-3>", self.on_rem)
         
         # Demais elementos da interface
         title = tk.Label(frame, text='idk')
-        btn_add = tk.Button(frame, text="Adicionar Ponto", width=20, command=lambda: self.on_add_edit(self.add_point))
-        btn_edit = tk.Button(frame, text="Editar Ponto(s)", width=20, command=lambda: self.on_add_edit(self.edit_point))
-        btn_rem = tk.Button(frame, text="Remover Ponto(s)", width=20, command=self.on_rem)
-        btn_gen = tk.Button(frame, text="Gerar Pontos", width=20, command=self.on_gen)
-        btn_save = tk.Button(frame, text="Salvar em JSON", width=20, command=self.on_save)
-        btn_load = tk.Button(frame, text="Carregar JSON", width=20, command=self.on_load)
+        btn_add = tk.Button(frame, text="Adicionar Ponto", width=15, command=self.on_add)
+        btn_rem = tk.Button(frame, text="Remover Ponto(s)", width=15, command=self.on_rem)
+        btn_gen = tk.Button(frame, text="Gerar Pontos", width=31, command=self.on_gen)
+        btn_save = tk.Button(frame, text="Salvar em JSON", width=15, command=self.on_save)
+        btn_load = tk.Button(frame, text="Carregar JSON", width=15, command=self.on_load)
         btn_start = tk.Button(frame, text="Iniciar Algoritmo", width=30, command=self.on_start)
         btn_exit = tk.Button(frame, text="Sair", width=30, command=self.root.destroy)
 
         # Ajuste dos elementos no grid
-        title.grid(row=0, column=1)
-        btn_add.grid(row=4, column=0, padx=5, pady=5, sticky='w')
-        btn_edit.grid(row=5, column=0, padx=5, pady=5, sticky='w')
-        btn_rem.grid(row=6, column=0, padx=5, pady=5, sticky='w')
-        btn_gen.grid(row=4, column=0, padx=5, pady=5, sticky='e')
-        btn_save.grid(row=5, column=0, padx=5, pady=5, sticky='e')
-        btn_load.grid(row=6, column=0, padx=5, pady=5, sticky='e')
+        title.grid(row=0, column=1, sticky='n')
+        btn_add.grid(row=4, column=0, pady=5, sticky='w')
+        btn_rem.grid(row=4, column=0, padx=114, pady=5, sticky='w')
+        btn_gen.grid(row=5, column=0, padx=1,pady=5, sticky='w')
+        btn_save.grid(row=6, column=0, pady=5, sticky='w')
+        btn_load.grid(row=6, column=0, padx=114, pady=5, sticky='w')
         btn_start.grid(row=4, column=2, padx=5, pady=5)
         btn_exit.grid(row=6, column=2, padx=5, pady=5)
 
     
-    
-
-    def on_add_edit(self, action, event=None):
+    def on_add(self, event=None):
         if len(self.points) == self.max:
             messagebox.showwarning('Erro', f'Você atingiu o número máximo de pontos ({self.max})!')
             return
+        
+        canvas_x, canvas_y = (event.x, event.y) if event else (self.w, self.h)
+        x, y, name = (canvas_x-self.w), (self.h-canvas_y), self.default_name()
 
-        canvas_x, canvas_y = (event.x, event.y) if event else (self.w/2, self.h/2)
+        self.edit_window(x, y, name, 'add', event)
+        
+
+    def on_edit(self, event):
+        click = self.click(event)
+        if not click: return
+
+        x, y = self.points[click][0], self.points[click][1]
+        self.edit_window(x, y, click, 'edit', event)
+    
+
+    def edit_window(self, x, y, old_name, action, event=None):
+        x, y = tk.StringVar(value=x), tk.StringVar(value=y)
+        new_name = tk.StringVar(value=old_name)
+        if action == 'add':
+            string, old_name = 'Adicionar', None
+        elif action == 'edit':
+            string = 'Editar'
+
         win_pos = f'+{event.x_root-50}+{event.y_root-150}' if event else None
-        x, y, name = int(canvas_x - self.w/2), int(self.h/2 - canvas_y), self.default_name()
-
         win = tk.Toplevel(self.root)
         win.geometry(win_pos)
-        win.title(f"Adicionar Ponto")
+        win.title(f"{string} Ponto")
         win.resizable(False, False)
         win.grab_set()
         
         frame = tk.Frame(win, padx=8, pady=8)
         frame.pack()
-                
+
         def validate_str(char, string):
             return len(string) <= 20 or char == ''
-        
+
         def validate_num(char, string):
-            if (len(string) <= 9) or (char == ''):
-                if (char.isdigit()) or (char=='.' and string.count('.')==0) or (char==''):
-                    return True
-            return False
+            try: return (len(string) <= 10) and ((not string) or float(string+'1'))
+            except ValueError: return False
 
         val_str, val_num = win.register(validate_str), win.register(validate_num)
 
@@ -101,43 +114,44 @@ class App():
         tk.Label(frame, text="X   ").grid(row=2, column=0)
         tk.Label(frame, text="   Y").grid(row=2, column=1)
 
-        x, y, name = tk.StringVar(value=x), tk.StringVar(value=y), tk.StringVar(value=name)
-        tk.Entry(frame, width=21, textvariable=name, validate="key", validatecommand=(val_str, "%S", "%P")) \
+        tk.Entry(frame, width=21, textvariable=new_name, validate="key", validatecommand=(val_str, "%S", "%P")) \
             .grid(row=1, column=0, columnspan=2, pady=(0, 6))
         tk.Entry(frame, width=9, textvariable=x, validate="key", validatecommand=(val_num, "%S", "%P")) \
             .grid(row=3, column=0, pady=(0, 10), sticky='w')
         tk.Entry(frame, width=9, textvariable=y, validate="key", validatecommand=(val_num, "%S", "%P")) \
             .grid(row=3, column=1, pady=(0, 10), sticky='e')
 
-        tk.Button(frame, text="Adicionar", width=8, command=lambda: action(float(x.get()), float(y.get()), name.get(), win)) \
+        tk.Button(frame, text=string, width=8, command=lambda: self.add_point(x.get(), y.get(), new_name.get(), old_name, win)) \
             .grid(row=4, column=0, sticky='sw')
         tk.Button(frame, text="Voltar", width=8, command=win.destroy) \
             .grid(row=4, column=1, sticky='se')
 
 
-    def add_point(self, x, y, name, win):
-        if not self.verify_point(x, y, name, win): return
-        point_id, text_id = self.plot(x, y, name)
+    def add_point(self, x, y, new_name, old_name, win):
+        if not self.verify_point(x, y, new_name, win): return
+        x = int(x) if float(x).is_integer() else float(x) # Elimina casas decimais se for um int
+        y = int(y) if float(y).is_integer() else float(y) # Elimina casas decimais se for um int
 
-        self.points[name] = [x, y]
-        self.ids[name] = [point_id, text_id]
+        if old_name:
+            string = 'editado'
+            self.canvas.delete(self.ids[old_name][0]), self.canvas.delete(self.ids[old_name][1])
+            del self.points[old_name], self.ids[old_name]
+        else: string = 'adicionado'
+
+        point_id, text_id = self.plot(x, y, new_name)
+        self.points[new_name], self.ids[new_name] = [x, y], [point_id, text_id]
 
         win.destroy()
-        messagebox.showinfo('Ponto adicionado', f'O ponto "{name}" foi adicionado no plano.')
+        messagebox.showinfo(f'Ponto {string}', f'O ponto "{new_name}" foi {string} no plano.')
 
-
-    def edit_point(self, x, y, name, win):
-        pass
 
     def on_rem(self, event=None):
-        if event:
-            x, y = int(event.x-self.w/2), int(self.h/2-event.y)
-            for point in self.points.items():
-                if (x-5 <= point[1][0] <= x+5) and (y-5 <= point[1][1] <= y+5):
-                    self.canvas.delete(self.ids[point[0]][0]), self.canvas.delete(self.ids[point[0]][1])
-                    del self.points[point[0]], self.ids[point[0]]
-                    return
-        else:
+        click = self.click(event)
+
+        if click:
+            self.canvas.delete(self.ids[click][0]), self.canvas.delete(self.ids[click][1])
+            del self.points[click], self.ids[click]
+        elif not event:
             win = tk.Toplevel(self.root)
             win.title(f"Remover Ponto(s)")
             win.resizable(False, False)
@@ -178,28 +192,28 @@ class App():
                 refresh_points()
 
             tk.Label(frame, text='Selecione o(s) ponto(s)\npara remoção:').grid(row=0, column=0, columnspan=2)
-            tk.Button(frame, text="A", font=("Arial", 8), width=1, height=1, command=select_all).place(x=2, y=28, width=14, height=14)
+            tk.Button(frame, text="A", font=("Arial", 8), width=1, height=1, command=select_all).place(x=1, y=28, width=14, height=14)
             tk.Button(frame, text="Remover", width=10, command=delete_points).grid(row=2, column=0, sticky='sw')
             tk.Button(frame, text="Voltar", width=10, command=win.destroy).grid(row=2, column=1, sticky='se')
 
             refresh_points()
 
-            
 
     def on_start(self):
         pass
 
+
     def on_gen(self):
         n = simpledialog.askinteger('Gerar Pontos', 'Número de pontos:')
-        if (len(self.points)+n) >= self.max:
+        if (len(self.points)+n) > self.max:
             messagebox.showerror('Erro', f'Número máximo de pontos ({self.max}) estourado!')
             return
 
         points = self.points.values()
         for i in range(n):            
             while True:
-                x = random.randint(int(-self.w/2), int(self.w/2))
-                y = random.randint(int(-self.h/2), int(self.h/2))
+                x = random.randint(-self.w, self.w)
+                y = random.randint(-self.h, self.h)
                 if not self.check_conflict(x, y): break
 
             name = self.default_name()
@@ -207,10 +221,9 @@ class App():
             
             self.points[name] = [x, y]
             self.ids[name] = [point_id, text_id]
-        
-        messagebox.showinfo('Pontos Gerados', f'{n} novos pontos foram gerados no plano.')
 
-
+        if n==1: messagebox.showinfo('Ponto Gerado', f'{n} novo ponto foi gerado no plano.')
+        else: messagebox.showinfo('Pontos Gerados', f'{n} novos pontos foram gerados no plano.')
 
 
     def on_save(self):
@@ -240,47 +253,64 @@ class App():
             return
         
 
-
-
+    # Gera um nome "default" para um ponto
     def default_name(self, i = 1):
         while f'Ponto {i}' in self.points.keys(): i += 1
         return f'Ponto {i}'
-    
+
+
+    # Checa se já existe um ponto nessas coordenadas
+    def check_conflict(self, x, y):
+        return [x, y] in self.points.values()
+
+
+    # Plota um ponto e seu texto no plano
     def plot(self, x, y, name):
-        canvas_x, canvas_y = (x + self.w/2), (self.h/2 - y)
+        canvas_x, canvas_y = (x + self.w), (self.h - y)
         name_pos = self.name_fix(canvas_x, canvas_y)
 
         point_id = self.canvas.create_oval(canvas_x-5, canvas_y-5, canvas_x+5, canvas_y+5, fill="blue")
         text_id = self.canvas.create_text(canvas_x+name_pos[0], canvas_y+name_pos[1], angle=name_pos[2], text=name, font=("Arial", 8))
 
         return point_id, text_id
-    
-    def check_boundary(self, x, y):
-        return (x < -self.w/2) or (self.w/2 < x) or (y < -self.h/2) or (self.h/2 < y)
 
-    def check_conflict(self, x, y):
-        return [x, y] in self.points.values()
-    
-    def check_name(self, name):
-        return name in self.points.keys()
-    
+
+    # Verifica se 'x', 'y' e 'name' são válidas no contexto do sistema
     def verify_point(self, x, y, name, win):
-        if self.check_boundary(x, y):
+        try: x, y = float(x), float(y)
+        except ValueError:
+            return not messagebox.showwarning('Erro', f'Digite coordenadas válidas para o ponto.', parent=win)
+
+        if (-self.w > x > self.w) or (-self.h > y > self.h):
             messagebox.showwarning('Erro', f'As coordenadas ({x}, {y}) estão fora do limite do plano.', parent=win)
         elif self.check_conflict(x, y):
             messagebox.showwarning('Erro', f'O ponto ({x}, {y}) já existe no plano.', parent=win)
-        elif self.check_name(name):
+        elif name in self.points.keys():
             messagebox.showwarning('Erro', f'Já existe um ponto com o nome "{name}".', parent=win)
+        elif not name:
+            messagebox.showwarning('Erro', f'Digite um nome válido para o ponto.', parent=win)
         else:
             return True
-        return False
-        
 
 
+    # Conserta a posição do nome no canvas
     def name_fix(self, x, y):
-        name_pos = [0, -10, 0] # [x, y, angle]
-        if x > self.w-10:  name_pos[:] = [-10, 0, 90]
-        if x < 10:         name_pos[:] = [+10, 0, -90]
-        if y < 13:         name_pos[1] = +10
+        name_pos = [0, -10, 0] # Valores padrão: [x, y, angle]
+
+        # Se estiver nas bordas horizontais (vira o nome)
+        if x > 2*self.w-10:  name_pos[:] = [-10, 0, 90]
+        if x < 10:           name_pos[:] = [+10, 0, -90]
+        # Se estiver na borda superior (projeta o nome em baixo)
+        if y < 13:           name_pos[1] = +10
 
         return name_pos
+
+
+    # Detecta se há algum ponto onde o usuário clicou
+    def click(self, event):
+        if not event: return
+
+        x, y = (event.x-self.w), (self.h-event.y)
+        for point in self.points.items():
+            if (x-5 <= point[1][0] <= x+5) and (y-5 <= point[1][1] <= y+5):
+                return point[0]
